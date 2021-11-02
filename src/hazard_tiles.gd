18cf9c29322b8e75420b7b81d961cca05475b9d8
Branden Akana <branden.akana@gmail.com>
2021-11-02 04:30:37 -1000

    change hazard tiles respawn point method

--- src/hazard_tiles.gd
+++ src/hazard_tiles.gd
@@ -1,13 +1,14 @@
 extends TileMap
 
 export (int) var damage = 100
-export (NodePath) var respawn_point
 
 # Gets the respawn point to send players after they die to this hazard.
 #
 # If no respawn point exists, the player will respawn at the start point.
 func get_respawn_point() -> Vector2:
-    if respawn_point:
-        return get_node(respawn_point).global_position
-    else:
+    if get_node_or_null("respawn"):
+        return $"respawn".global_position
+    elif not Engine.editor_hint:
         return Game.get_start_point()
+    else:
+        return Vector2.ZERO
