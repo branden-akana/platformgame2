9afb250e441a157f0f67b756eb63a8715b751fd1
Branden Akana <branden.akana@gmail.com>
2022-11-18 19:42:54 -1000

    Work on changing background visuals

--- backgrounds/3d_background.gd
+++ backgrounds/3d_background.gd
@@ -8,27 +8,24 @@ extends Sprite2D
 
 var camera
 
-func _ready():
-	top_level = true
-
 func _process(delta):
 
 	if not Engine.is_editor_hint() and GameState.is_inside_tree():
 		camera = GameState.get_camera()
 
-	var box_1 = $viewport/spatial/csg_box
-	var box_2 = $viewport/spatial/csg_box_2
+	# var box_1 = $viewport/spatial/csg_box
+	# var box_2 = $viewport/spatial/csg_box_2
 
-	var z = scroll_rate * delta
-	if camera:
-		var x = origin.x + camera.focus.x * parallax * 0.0005
-		position = origin + camera.focus
+	# var z = scroll_rate * delta
+	# if camera:
+	# 	var x = origin.x + camera.focus.x * parallax * 0.0005
+	# 	position = origin + camera.focus
 
-		box_1.material.uv1_offset.x = fmod(-x, 1.0)
-		box_2.material.uv1_offset.x = fmod(-x, 1.0)
+	# 	box_1.material.uv1_offset.x = fmod(-x, 1.0)
+	# 	box_2.material.uv1_offset.x = fmod(-x, 1.0)
 
-	box_1.material.uv1_offset.z = fmod(box_1.material.uv1_offset.z + z, 1.0)
-	box_2.material.uv1_offset.z = fmod(box_2.material.uv1_offset.z + z, 1.0)
+	# box_1.material.uv1_offset.z = fmod(box_1.material.uv1_offset.z + z, 1.0)
+	# box_2.material.uv1_offset.z = fmod(box_2.material.uv1_offset.z + z, 1.0)
 
 	
 	
