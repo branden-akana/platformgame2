495c41836d750905edc954f33a391d3a34d4fd1d
Branden Akana <branden.akana@gmail.com>
2022-11-18 19:47:18 -1000

    Remove level from selection menu due to being unable to open

--- ui/menu/select_level.gd
+++ ui/menu/select_level.gd
@@ -4,53 +4,43 @@ extends MenuSelection
 const Level_TestHub = preload("res://levels/test_area.tscn")
 const Level_TestTutorial = preload("res://levels/test_tutorial.tscn")
 const Level_Test1 = preload("res://levels/test.tscn")
-const Level_Test2 = preload("res://levels/test2.tscn")
 
 class Hub extends MenuSelection:
-    func get_label(): return "test area"
-    func get_extra():
-        return "best: %s" % GameState.run_timer.get_record("test area")
-    func on_select(menu):
-        GameState.load_scene(Level_TestHub)
-        menu.hide()
+	func get_label(): return "test area"
+	func get_extra():
+		return "best: %s" % GameState.run_timer.get_record("test area")
+	func on_select(menu):
+		GameState.load_scene(Level_TestHub)
+		menu.hide()
 
 class Tutorial extends MenuSelection:
-    func get_label(): return "test tutorial"
-    func get_extra():
-        return "best: %s" % GameState.run_timer.get_record("test tutorial")
-    func on_select(menu):
-        GameState.load_scene(Level_TestTutorial)
-        menu.hide()
+	func get_label(): return "test tutorial"
+	func get_extra():
+		return "best: %s" % GameState.run_timer.get_record("test tutorial")
+	func on_select(menu):
+		GameState.load_scene(Level_TestTutorial)
+		menu.hide()
 
 class Test1 extends MenuSelection:
-    func get_label(): return "level 1"
-    func get_extra():
-        return "best: %s" % GameState.run_timer.get_record("level 1")
-    func on_select(menu):
-        GameState.load_scene(Level_Test1)
-        menu.hide()
-
-class Test2 extends MenuSelection:
-    func get_label(): return "level 2 wip"
-    func get_extra():
-        return "best: %s" % GameState.run_timer.get_record("level 2")
-    func on_select(menu):
-        GameState.load_scene(Level_Test2)
-        menu.hide()
+	func get_label(): return "level 1"
+	func get_extra():
+		return "best: %s" % GameState.run_timer.get_record("level 1")
+	func on_select(menu):
+		GameState.load_scene(Level_Test1)
+		menu.hide()
 
 var items = [
-    Tutorial.new(),
-    Test1.new(),
-    Test2.new(),
-    Hub.new(),
-    SelectReturn.new()
+	Tutorial.new(),
+	Test1.new(),
+	Hub.new(),
+	SelectReturn.new()
 ]
 
 func get_items():
-    return items
+	return items
 
 func get_label():
-    return "select level"
+	return "select level"
 
 func on_select(menu):
-    menu.set_menu(self)
+	menu.set_menu(self)
