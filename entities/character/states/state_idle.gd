f96c0516eea5b6a6b1bd509ef2a0208df53452ff
Branden Akana <branden.akana@gmail.com>
2022-12-05 15:35:02 -1000

    IdleState: only start dash when speed is under a threshold

--- entities/character/states/state_idle.gd
+++ entities/character/states/state_idle.gd
@@ -4,8 +4,10 @@ func on_update(delta, _fsm):
 
 	character._friction(delta)
 
-	if character.input.holding_left() and not character.input.holding_right():
-		character.action_dash(Direction.LEFT)
+	if abs(character.velocity.x) <= 200:
 
-	elif character.input.holding_right() and not character.input.holding_left():
-		character.action_dash(Direction.RIGHT)
+		if character.input.holding_left() and not character.input.holding_right():
+			character.action_dash(Direction.LEFT)
+
+		elif character.input.holding_right() and not character.input.holding_left():
+			character.action_dash(Direction.RIGHT)
